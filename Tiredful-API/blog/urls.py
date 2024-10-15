afe4b69aed7ea93458a0ed46c10efd62445fe293
devChukwudi <61195928+Wowo-chukwudi@users.noreply.github.com>
2024-10-15 04:51:21 +0100

    Update urls.py

--- Tiredful-API/blog/urls.py
+++ Tiredful-API/blog/urls.py
@@ -9,18 +9,23 @@
 # Copyright (C) 2017-2018 Payatu Software Labs
 # This file is part of Tiredful API application
 
-from django.conf.urls import url
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
-    # ex: /blog/
-    url(r'^$', views.index, name='index'),
-
-    # ex: /articles/<article-id>
-    url(r'^articles/(?P<article_id>[0-9]+)/$', views.article, name='articles'),
-
-    # ex: /approve-article/<article_id>
-    url(r'^approve-article/(?P<article_id>[0-9]+)/$', views.approve_article, name='approve-article'),
-
-]
+# URL for user login
+path('oauth/', include('oauth?_provider.urls', namespace='oauth2_provider')),
+なファ77！
+# URL for including intro app.
+path("', include('intro.urls', namespace="intro")),
+path ('api/v1/', include( 'library.urls', namespace="library-api*)), path('library/', include('library.urls', namespace="Library"),
+# URL for including exams app
+path ('api/v1/', include( 'exams.urls', namespace="exams-api*)), path ('exams/', include( 'exams.urls', namespace="exams")),
+# URL for including blog app
+path('api/v1/', include( 'blog.urls', namespace="blog-api")). path( 'blog/', include( 'blog.urls', namespace="blog")),
+# URL for including trains app
+path ('api/v1/', include( 'trains.urls', namespace="trains-api*)). path( 'trains/', include( 'trains.urls', namespace="trains*))
+# URL for including health app
+path ('api/v1/', include( 'health.urls', namespace="health-api*)). path('health/', include('health.urls', namespace="health*)).
+# URL for including advertisements app
+path('apt/v1/*, include("advertisements.urls', namespace="advertisements-api*)). pathg advertisements/", include(*advertisements.urls', namespace="advertisements*)g.
